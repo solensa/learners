eb66e592fb286480fb03ad9b19064d8ff6310a11
solensa <39489819+solensa@users.noreply.github.com>
2021-09-22 15:53:12 +0100

    update

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,32 @@
 var data = [
-  [000, ["reason1", "reason2"], ["con 1", "con2"]],
-  [001, ["reason3", "reason4"], ["con 3", "con4"]],
-  [002, ["reason3", "reason4"], ["con 3", "con4"]],
+  [
+    000,
+    [
+      "Strong appreciation of engineering and manufacture fundamentals to drive design decisions",
+      "Skills to load product capabilities into BIM and contribute to selection",
+      "Considers/tries inventing own tech solutions",
+      "Identifies problems/goals that tech could help to solve",
+    ],
+    [],
+  ],
+  [
+    001,
+    [
+      "Development of scheduling algorithms using an end to end systems approach to track and monitor progress",
+      "Computer generated scheduling and instructions ",
+      "development of work packages",
+    ],
+    [],
+  ],
+  [
+    002,
+    [
+      "Strong appreciation of engineering and manufacture fundamentals to drive design decisions",
+      "Skills to ask the right questions of materials (load etc) where answers will be generated from computer programmes",
+    ],
+    [
+      "Less need for physical testing ",
+      "product developers use BIM modelling tools to test and then final physical tests",
+    ],
+  ],
 ];
