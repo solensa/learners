656441191910bfb94f35601c86af5bb53d90888d
solensa <rob@okamistudio.com>
2021-09-27 13:37:26 +0100

    code done

---
+++ assets/js/data2.js
@@ -0,0 +1,32 @@
+var data2 = [
+  [
+    0,
+    [
+      "Strong appreciation of engineering and manufacture fundamentals to drive design decisions",
+      "Skills to load product capabilities into BIM and contribute to selection",
+      "Considers/tries inventing own tech solutions",
+      "Identifies problems/goals that tech could help to solve",
+    ],
+    [],
+  ],
+  [
+    1,
+    [
+      "Development of scheduling algorithms using an end to end systems approach to track and monitor progress",
+      "Computer generated scheduling and instructions ",
+      "development of work packages",
+    ],
+    [],
+  ],
+  [
+    2,
+    [
+      "Strong appreciation of engineering and manufacture fundamentals to drive design decisions",
+      "Skills to ask the right questions of materials (load etc) where answers will be generated from computer programmes",
+    ],
+    [
+      "Less need for physical testing ",
+      "product developers use BIM modelling tools to test and then final physical tests",
+    ],
+  ],
+];
--- assets/js/explore.js
+++ assets/js/explore.js
@@ -28,12 +28,32 @@ const unCheck = () => {
   for (i = 0; i < x.length; i++) {
     x[i].checked = false;
   }
-
   $(".radBtnsRoles").addClass("hide");
 };
 
 const returnHtml = (index) => {
   let matchFound = false;
+  let matchFound2 = false;
+
+  // check if function results box should be shown
+  for (let i = 0; i < data2.length; i++) {
+    if (data2[i][0] == index[1]) {
+      let str = "<ul>";
+      let str2 = "<ul>";
+      for (let j = 0; j < data2[i][1].length; j++) {
+        str += "<li>" + data2[i][1][j] + "</li>";
+      }
+      for (let k = 0; k < data2[i][2].length; k++) {
+        str2 += "<li>" + data2[i][2][k] + "</li>";
+      }
+      $("#growBullets2").html(str);
+      $("#shrinkBullets2").html(str2);
+      $("#resultsSection2").removeClass("hide");
+      matchFound2 = true;
+    }
+  }
+
+  //   check if roles results box should be shown
   for (let i = 0; i < data.length; i++) {
     if (data[i][0] == index) {
       let str = "<ul>";
@@ -52,6 +72,9 @@ const returnHtml = (index) => {
   }
   if (!matchFound) {
     $("#resultsSection").addClass("hide");
+  }
+  if (!matchFound2) {
+    $("#resultsSection2").addClass("hide");
   }
 };
 
